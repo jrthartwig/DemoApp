6cdae947f97d13f9ad017f56887b91fef6b46238
Jocelynn Hartwig <johartwig@microsoft.com>
2024-11-15 10:47:14 -0500

    initial project

---
+++ DemoApp.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoApp.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine() ?? string.Empty;
+            PrintGreeting(name);
+        }
+
+        public static void PrintGreeting(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ DemoApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using DemoApp.ConsoleApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace DemoApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
